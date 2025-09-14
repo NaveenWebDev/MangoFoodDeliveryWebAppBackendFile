4f5d057fb7608e9fa3b4c07014e4f2ba05942d0e
Naveen Sharma <98882301+NaveenWebDev@users.noreply.github.com>
2025-09-14 14:28:23 +0530

    add routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, signIn, signOut } = require('../controllers/auth.controllers');
+const { signUp, signIn, signOut, sendOtp, verifyOtp, resetPassword  } = require('../controllers/auth.controllers');
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.get('/signout', signOut);
+router.post('/send-otp', sendOtp);
+router.post('/verify-otp', verifyOtp);
+router.post('/reset-password', resetPassword);
+
 
 module.exports = router;
